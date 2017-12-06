c08d45b62b7b9af7a54c42185cecd9ac3c1bacb7
root <root@localhost.localdomain>
2017-12-06 20:30:47 +0100

    add md5 brut, make thr core in main file, add auxiliare script, add description

--- main.py
+++ main.py
@@ -4,80 +4,34 @@ David Cherednik:
 
 MAHE: multi-platform accessible hacker environment
 """
+# -*- coding: utf-8 -*- 
 import sys
-from directory_brut import g
-import directory_brut.g as h
+import auxiliary.description as d
+
+d.ASCII_ART()
+
+def main():
+	while True:
+		a = input('[MAHE] <=> ')
+		print(a)
+		if a == "-help" or a == "-h":
+			# print(d.help())
+			f = open('auxiliary/description.txt', 'r')
+			print (f.read())	
+		if a == 'exit': break
+
+main()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import urllib
-# import threading
-# from datetime import datetime
-# import sys
-# import os
 # import time
-# event = threading.Event()
-# event2 = threading.Event()
-# n = 2
-# def func(ev):
-# 	File = open("dict1.txt", 'r')
-# 	for line in File:
-# 		a = 'http://maulnet.ru/' + line
-# 		url=urllib.urlopen(a)
-# 		if url.code == 200:
-# 			print a, url.code, "--Ditrctoty available"
-# 			print "-------*--------"
-# 		elif url.code == 404:
-# 			print a, url.code ,"--error"
-# 			print "-------*--------"
-
-
-# def func2(ev):
-# 	File = open("dict2.txt", 'r')
-	
-# 	for line in File:
-# 		a = 'http://maulnet.ru/' + line
-# 		url=urllib.urlopen(a)
-# 		if url.code == 200:
-# 			print a, url.code, "--Ditrctoty available"
-# 			print "-------*--------"
-# 		elif url.code == 404:
-# 			print a, url.code ,"--error"
-# 			print "-------*--------"
- 
-
-# start_time = datetime.now()
-# thr = threading.Thread(target=func, args=(event, ))
-# thr2 = threading.Thread(target=func2, args=(event2, ))
-
-# thr.daemon = True
-# thr2.daemon = True
- 
-# thr.start()
-# thr2.start()
-
-# while True: #
-#     thr.join(600)
-#     thr.join(600)
-#     if not thr.isAlive() and not thr2.isAlive():
-#         break
-# end_time = datetime.now()
-# print('Duration: {}'.format(end_time - start_time))
+# import sys
+# animation = "|/-\\"
+# for i in range(100):
+#     time.sleep(0.01)
+#     sys.stdout.write(animation[i % len(animation)])
+#     sys.stdout.flush()
+# print("End!")
 
 
 
---
+++ md5_brut/md5_main.py
@@ -0,0 +1,33 @@
+# import md5
+import hashlib
+
+counter = 0
+
+def md5_brut():
+	pass_in = input("Enter the MD5 Hash: ")
+	pwfile = input("Enter the Password Dictionary: ")
+
+	try:
+		pwfile = open(pwfile, "r", encoding='utf-8')
+
+	except:
+		print("Dictionary doesn't find or doesn't exist.")
+
+	for password in pwfile:
+		fileemd5 = hashlib.md5(password.encode()).hexdigest()
+		print("trying password number %s" %(password.strip()))		
+		# print("trying password number %d %s" %(counter, password.strip()))		
+		# counter += 1
+
+		if pass_in == fileemd5:
+			print("\n Match found. \n Password is: %s" % password)
+			break
+
+		else:
+			print("-----password not found")
+
+md5_brut()
+# df3939f11965e7e75dbc046cd9af1c67  == dad
+# /root/prog/pycode/MAHE/md5_brut/dict.txt
+
+
