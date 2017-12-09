1aad8b6505403c219a73f7ff730ba3ed788ca15d
default081 <vadim.snake96@mail.ru>
2017-12-09 11:00:28 +0100

    mofiaid admin brut

--- admin_serach/admin.py
+++ admin_serach/admin.py
@@ -7,44 +7,50 @@ event = threading.Event()
 event2 = threading.Event()
 
 def brut_part_1(ev):
-	File = open("../auxiliary/dictionary/admin-panels.txt", "r")
+	File = open("../auxiliary/dictionary/admin-panels1.txt", "r")
 	for line in File:
 		addres = 'http://maulnet.ru/' + line
 		try:
 			url = urllib.request.urlopen(addres)
 			if(url.code == 200):
 				print(addres, url.code, "--Admin panel find")
+				print ("-------*--------")
 		except urllib.error.HTTPError as error:
-			pass
+			if(error.code != 200):
+				print(addres, error.code, "--Error")
+				print ("-------*--------")
 
 
-# def brut_part_two(ev):
-# 	File = open("auxiliary/dictionary/admin-panels.txt", "r")
-# 	for line in File:
-# 		addres = 'http://maulnet.ru/' + line
-# 		try:
-# 			url = urllib.request.urlopen(address)
-# 			if(url.code == 200):
-# 				print(address, url.code, "--Admin panel find")
-# 		except urllib.error.HTTPError as error:
-# 			pass
+def brut_part_two(ev):
+	File = open("../auxiliary/dictionary/admin-panels2.txt", "r")
+	for line in File:
+		addres = 'http://maulnet.ru/' + line
+		try:
+			url = urllib.request.urlopen(addres)
+			if(url.code == 200):
+				print(addres, url.code, "--Admin panel find")
+				print ("-------*--------")
+		except urllib.error.HTTPError as error:
+			if(error.code != 200):
+				print(addres, error.code, "--Error")
+				print ("-------*--------")
 
 
 def admin_main():
 	start_time = datetime.now()
 
 	thr = threading.Thread(target=brut_part_1, args=(event, )) # initiate threading n1
-	# thr2 = threading.Thread(target=func2, args=(event2, )) #initiate threading n2
+	thr2 = threading.Thread(target=brut_part_two, args=(event2, )) #initiate threading n2
 
 	thr.daemon = True
-	# thr2.daemon = True
+	thr2.daemon = True
 	 
 	thr.start()
-	# thr2.start()
+	thr2.start()
 
 	while True: #
 	    thr.join(600)
-	    # thr.join(600)
+	    thr.join(600)
 	    if not thr.isAlive():
 	        break
 
