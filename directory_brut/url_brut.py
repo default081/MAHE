2b4438202047569d7b7841fe2a1af48786fc81ab
bat <bat@localhost.localdomain>
2018-01-07 21:15:18 +0100

    add url brut counter

--- directory_brut/url_brut.py
+++ directory_brut/url_brut.py
@@ -8,6 +8,10 @@ event = threading.Event()
 event2 = threading.Event()
 
 def func(ev, urll):
+
+	global thread1_directoty
+	thread1_directoty = 0
+
 	File = open("dictionare/dict1.txt", 'r')
 	for line in File:
 		address = urll + line
@@ -16,6 +20,7 @@ def func(ev, urll):
 			if url.code == 200:
 				print (address, url.code, "--Ditrctoty available")
 				print ("-------*--------")
+				thread1_directoty += 1
 		except urllib.error.HTTPError as error:
 			if(error.code == 404):
 				pass
@@ -25,6 +30,10 @@ def func(ev, urll):
 
 
 def func2(ev, urll):
+
+	global thread2_directoty
+	thread2_directoty = 0
+
 	File = open("dictionare/dict2.txt", 'r')
 	
 	for line in File:
@@ -33,6 +42,7 @@ def func2(ev, urll):
 			url = urllib.request.urlopen(address)
 			print (address, url.code, "--Ditrctoty available")
 			print ("-------*--------")
+			thread2_directoty += 1
 		except urllib.error.HTTPError as error:
 			if(error.code == 404):
 				pass
@@ -44,6 +54,7 @@ def call():
 	start_time = datetime.now()
 
 	a = input("-> Enter Url: ")
+	print()
 
 	thr = threading.Thread(target=func, args=(event, a)) # initiate threading n1
 	thr2 = threading.Thread(target=func2, args=(event2, a)) #initiate threading n2
@@ -56,15 +67,19 @@ def call():
 
 	while True: #
 	    thr.join(600)
-	    thr.join(600)
+	    # thr.join(600)
 	    if not thr.isAlive() and not thr2.isAlive():
 	        break
 
 	end_time = datetime.now()
-	print('Duration time: {}'.format(end_time - start_time) + "\n")
+	print('Duration time: {}'.format(end_time - start_time))
 
-# call()
+	all_find_direct = thread1_directoty + thread2_directoty
+	print("find " + str(all_find_direct) + " open directoris")
+	print()
+call()
 
+# http://192.168.1.142/
 # -----------------------------------
 # import urllib.request
 # import threading
